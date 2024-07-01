82fb21fc7558c7e26256e0990b0f503c501047b8
angelgaibor <mat.angel.gaibor@gmail.com>
2024-07-01 12:14:44 -0500

    AG: cambio en codificación marco en DMQ upm y selección de ENIGHUR replicada correctamente

--- rutinas/01_general/01_variables_necesarias.R
+++ rutinas/01_general/01_variables_necesarias.R
@@ -14,5 +14,32 @@ marco_01 <- marco_upm %>%
          domgeo = substr(domest, 1, 2)) |> 
   select(id_upm, pro, area, domgeo, domest, estrato, Mi, nap)
 
-saveRDS(marco_01, "productos/01_general/marco_upm_01.rds")
+# Se generó un error en la codificación de las UPM de Quito por lo que se corrige en el marco de upm y
+# en el archivo de correspondencia id_upm mansec
 
+apoyo <- marco_01 |> 
+  filter(substr(id_upm, 1, 7) %in% c("1701509", "1701809")) |>
+  group_by(id_par = substr(id_upm, 1, 6)) |> 
+  mutate(cong = row_number()) |> 
+  ungroup() |> 
+  mutate(id_upm_cor = paste0(id_par, "9", str_pad(cong, 3, "left", "0"), "01")) 
+
+marco_02 <- marco_01 |> 
+  filter(!substr(id_upm, 1, 7) %in% c("1701509", "1701809")) |> 
+  rbind(apoyo|> 
+          select(id_upm = id_upm_cor, pro, area, domgeo, domest, estrato, Mi, nap)) |> 
+  arrange(pro, estrato)
+
+n_distinct(marco_02$id_upm) == dim(marco_02)[1]
+
+saveRDS(marco_02, "productos/01_general/marco_upm_01.rds")
+
+# man_sec_upm_final_dmq <- readRDS("D:/MAG/marco_upm/productos/02_conglomeracion/man_sec_upm_final_dmq.rds")
+# 
+# man_sec_upm_final_corregido <- man_sec_upm_final_dmq |> 
+#   left_join(apoyo |> select(id_upm, id_upm_cor), by = "id_upm") |> 
+#   mutate(id_upm_final = ifelse(!is.na(id_upm_cor), id_upm_cor, id_upm)) |> 
+#   select(id_upm_final, man_sec, viv_ocu) |> 
+#   rename(id_upm = id_upm_final)
+# 
+# saveRDS(man_sec_upm_final_corregido, "insumos/01_general/man_sec_upm.rds")
