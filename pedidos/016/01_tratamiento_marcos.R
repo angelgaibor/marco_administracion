f2a7e6350b2c788dc5bae8e96b2e427e6f06bdaf
angelgaibor <mat.angel.gaibor@gmail.com>
2025-04-07 14:20:51 -0500

    tablas marcos

--- pedidos/016/01_tratamiento_marcos.R
+++ pedidos/016/01_tratamiento_marcos.R
@@ -54,5 +54,37 @@ tabla02 <- marco10 %>%
           mutate(marco = "2022") %>% 
           select(marco, pro, est, viv, porviv, upm, porupm))
 
-export(list(tabla01, tabla02), "pedidos/014/tablas_marco_10_22.xlsx")  
+tabla03_01 <- marco10 %>% 
+  group_by(pro = substr(id_upm, 1, 2), estrato) %>% 
+  summarise(viv = sum(viv),
+            upm = n()) %>% 
+  group_by(pro) |> 
+  mutate(porviv = round(100 * viv / sum(viv), 2),
+         porupm = round(100 * upm / sum(upm), 2)) %>% 
+  mutate(marco = "2010") %>% 
+  select(marco, pro, estrato, viv, porviv, upm, porupm) %>% 
+  rbind(marco22 %>% 
+          group_by(pro = substr(id_upm, 1, 2), estrato) %>% 
+          summarise(viv = sum(viv),
+                    upm = n()) %>% 
+          group_by(pro) |> 
+          mutate(porviv = round(100 * viv / sum(viv), 2),
+                 porupm = round(100 * upm / sum(upm), 2)) %>% 
+          ungroup() |> 
+          mutate(marco = "2022") %>% 
+          select(marco, pro, estrato, viv, porviv, upm, porupm))
+
+lol <- tabla03_01 |> 
+  group_by(marco, pro, estrato = "Total") |> 
+  summarise(viv = sum(viv),
+            porviv = sum(porviv),
+            upm = sum(upm),
+            porupm = sum(porupm))
+
+tabla03 <- tabla03_01 |> 
+  rbind(lol) |> 
+  arrange(marco, pro)
+
+export(list("area" = tabla01, "provincia" = tabla02, "estrato" = tabla03), 
+       "pedidos/016/tablas_marco_10_22.xlsx")  
   
