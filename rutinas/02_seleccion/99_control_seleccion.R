a53c63b6fc41b947d7ff9b76faa0ee302bce978e
angelgaibor <mat.angel.gaibor@gmail.com>
2025-06-19 11:51:59 -0500

    revisión muestra steps

---
+++ rutinas/02_seleccion/99_control_seleccion.R
@@ -0,0 +1,25 @@
+rm(list = ls())
+
+library(tidyverse)
+
+nombre <- last(list.files("productos/01_general/", pattern = ".rds"))
+
+marco <- readRDS(paste0("productos/01_general/",nombre))
+
+encuesta <- "steps_202506"
+
+var <- names(marco)[grepl("_sel" ,names(marco)) & !grepl("_seleccionable" ,names(marco)) & !grepl(encuesta ,names(marco))]
+
+for(i in 1:length(var)){
+  lol1 <- var[i]
+  lol2 <- paste0(encuesta, "_sel")
+  
+  aux <- marco |> 
+    group_by(.data[[lol1]], .data[[lol2]]) |> 
+    summarise(n = n()) |> 
+    pivot_wider(names_from = .data[[lol1]], 
+                values_from = n, 
+                names_prefix = paste0(var[i], "_"),
+                values_fill = 0)
+  print(aux)
+}
